db4dfe4f537ef4808f666e1d5c677049d0ff23f3
lucas271102 <lucas001spe@gmail.com>
2023-06-28 18:05:37 -0300

    faltaRutasYMiddlewares

---
+++ schemas/users.js
@@ -0,0 +1,6 @@
+import joi from 'joi'
+
+export const userCreateSignIn= joi.object({
+    email:joi.string().email({minDomainSegments:2}).required(),
+    password:joi.string().min(8).max(25).required(),
+})
