5c880b67622820d7c4ed852a7b5f55ff56e3824f
lucas271102 <lucas001spe@gmail.com>
2023-06-29 11:26:09 -0300

    scSignup

--- schemas/users.js
+++ schemas/users.js
@@ -4,3 +4,9 @@ export const userCreateSignIn= joi.object({
     email:joi.string().email({minDomainSegments:2}).required(),
     password:joi.string().min(8).max(25).required(),
 })
+export const userCreateSignUp = joi.object({
+    email:joi.string().email({minDomainSegments:2}).required(),
+    password:joi.string().min(8).max(25).required,
+    profilePicture:joi.string()
+
+})
