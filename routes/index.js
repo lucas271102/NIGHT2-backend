b22ba20ca7e79714f17d0049e3b52a72b4449fe4
lucas271102 <lucas001spe@gmail.com>
2023-07-04 16:25:29 -0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import userRouter from './users.js'
 var router = Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Coming soon: Night2Events' });
