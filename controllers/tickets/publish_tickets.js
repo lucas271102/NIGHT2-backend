8ad2a63e7be873936d9cacdbe3bfb400129c3a6c
lucas271102 <lucas001spe@gmail.com>
2023-07-09 17:56:31 -0300

    publish

--- controllers/tickets/publish_tickets.js
+++ controllers/tickets/publish_tickets.js
@@ -1,1 +1,19 @@
-import Tickets from "../../models/Tickets";
+import Tickets from "../../models/Tickets.js";
+let publish_tickets= async ( req, res, next)=>{
+    req.body.name
+    req.body.photo
+    req.body.price
+    req.body.category_id
+    try {
+        await Tickets.create(req.body)
+        return res.status(201).json({
+            success:true,
+            message:'Your ticket was successfully published!',
+            data: req.body
+
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+export default publish_tickets
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,8 @@
 import express from "express";
 import get_tickets from "../controllers/tickets/get_tickets.js";
+import publish_tickets from "../controllers/tickets/publish_tickets.js";
 const router = express.Router();
 
   router.get('/', get_tickets)
+  router.post('/publish', publish_tickets)
   export default router 
