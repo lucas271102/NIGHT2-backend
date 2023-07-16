2e977988c3ba6a7ce909e92907f0ac97bd48f95f
lucas271102 <lucas001spe@gmail.com>
2023-07-15 21:48:59 -0300

    m

--- controllers/cart/createAndUpdate.js
+++ controllers/cart/createAndUpdate.js
@@ -1,2 +1,12 @@
 import Tickets from '../../models/Tickets.js'
 import Cart from '../../models/Cart.js'
+
+
+const addAndUpdate = async(req, res)=>{
+    try {
+        const {ticketId}= req.body
+        let ticket = awa
+    } catch (error) {
+        
+    }
+}
