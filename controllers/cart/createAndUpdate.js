9e7b62b2a9a7c1398ffadc9cfd3ca2c0f7b37ee7
lucas271102 <lucas001spe@gmail.com>
2023-07-16 21:56:11 -0300

    falta

--- controllers/cart/createAndUpdate.js
+++ controllers/cart/createAndUpdate.js
@@ -1,12 +1,36 @@
 import Tickets from '../../models/Tickets.js'
 import Cart from '../../models/Cart.js'
-
-
-const addAndUpdate = async(req, res)=>{
+const addToCart= async (req, res)=>{
     try {
-        const {ticketId}= req.body
-        let ticket = awa
+        const {ticketId} = req.body;
+        
+        let ticket = await Tickets.findOne({_id: ticketId})
+        if (ticket){
+            let cart = await Cart .findOne({ticket_id:ticketId});
+            if(!cart){
+                const data  = {
+                    ticket_id: ticketId,
+                    quantity: 1
+                }
+                let one = await Cart.create(data)
+                one = await one.populate('ticket_id')
+                const oneFilter={
+                    ticket_id:one.ticket_id,
+                    quantity:one.quantity
+                }
+                if(one){
+                    return res.status(201).json({
+                        one: oneFilter,
+                        message:"Ticket added to cart.",
+                        timestamps: one.createdAt.getUTCDate()
+                    })
+
+                }
+            }
+        }
+
     } catch (error) {
         
     }
 }
+
