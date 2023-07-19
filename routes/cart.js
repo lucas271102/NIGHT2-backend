761f480484842a9c4c85e7feb71eb149ceca4ee8
lucas271102 <lucas001spe@gmail.com>
2023-07-19 15:24:27 -0300

    routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,2 +1,10 @@
 import  express from "express";
 const router = express.Router()
+import createCart from '../controllers/cart/createAndUpdate.js'
+import deleteOne from '../controllers/cart/deleteOne.js'
+import viewCart from '../controllers/cart/view.js'
+  
+
+router.post('/createcart', createCart)
+router.get('/', viewCart)
+router.delete('/', deleteOne) 
