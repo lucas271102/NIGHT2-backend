752cea6f8621f9007dff6f3aec25ed088b08a6b2
lucas271102 <lucas001spe@gmail.com>
2023-07-26 14:27:33 -0300

    addtocart

--- routes/cart.js
+++ routes/cart.js
@@ -6,6 +6,6 @@ import viewCart from '../controllers/cart/view.js'
   
 
 router.post('/createcart', createCart)
-router.get('cart/', viewCart)
+router.get('/cart', viewCart)
 router.delete('/', deleteOne) 
 export default router
